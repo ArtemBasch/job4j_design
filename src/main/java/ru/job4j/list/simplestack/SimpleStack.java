3e33bc5cff31bd0744e3ac12e0dd41450621a905
Art <bashlykovartem@gmail.com>
2022-06-14 22:28:01 +0300

    упростил addFirst

--- src/main/java/ru/job4j/list/simplestack/SimpleStack.java
+++ src/main/java/ru/job4j/list/simplestack/SimpleStack.java
@@ -1,16 +1,18 @@
-//package ru.job4j.list.simplestack;
-//
-//import ru.job4j.list.forwardlikned.ForwardLinked;
-//
-//public class SimpleStack<T> {
-//
-//    private ForwardLinked<T> linked = new ForwardLinked<T>();
-//
-//    public T pop() {
-//        return linked.deleteFirst();
-//    }
-//
-//    public void push(T value) {
-//        linked.addFirst(value);
-//    }
-//}
+/**
+package ru.job4j.list.simplestack;
+
+import ru.job4j.list.forwardlikned.ForwardLinked;
+
+public class SimpleStack<T> {
+
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public T pop() {
+        return linked.deleteFirst();
+    }
+
+    public void push(T value) {
+        linked.addFirst(value);
+    }
+}
+*/
