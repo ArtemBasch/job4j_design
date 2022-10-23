fdf50e71d99f7eb4d550a77ee1a5d6675a23d6ad
AremBasch <bashlykovartem@gmail.com>
2022-10-23 22:45:40 +0300

    FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/inputoutput/fileoutputstream/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.inputoutput.fileoutputstream;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for(int i = 1; i <= 9; i++) {
+                for(int j = 1; j <= 9; j++) {
+                    out.write((i + " x " + j + " = " + (i*j)).getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
