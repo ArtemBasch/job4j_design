2f3a88de354ded7d809b473862dc7cc2d669e8bc
AremBasch <bashlykovartem@gmail.com>
2022-10-23 22:48:03 +0300

    FileOutputStream. [#252488]

--- src/main/java/ru/job4j/inputoutput/fileoutputstream/ResultFile.java
+++ src/main/java/ru/job4j/inputoutput/fileoutputstream/ResultFile.java
@@ -5,9 +5,9 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for(int i = 1; i <= 9; i++) {
-                for(int j = 1; j <= 9; j++) {
-                    out.write((i + " x " + j + " = " + (i*j)).getBytes());
+            for (int i = 1; i <= 9; i++) {
+                for (int j = 1; j <= 9; j++) {
+                    out.write((i + " x " + j + " = " + (i * j)).getBytes());
                     out.write(System.lineSeparator().getBytes());
                 }
                 out.write(System.lineSeparator().getBytes());
