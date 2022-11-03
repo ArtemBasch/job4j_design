9a5b153c9b029bce9e29b2232495b5776e369c74
AremBasch <bashlykovartem@gmail.com>
2022-11-03 23:40:19 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/inputoutput/fileoutputstream/analysis/Analysis.java
@@ -0,0 +1,32 @@
+package ru.job4j.inputoutput.fileoutputstream.analysis;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        boolean isOn = true;
+
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            PrintWriter out = new PrintWriter(new FileOutputStream(target));
+            String line;
+            while ((line = in.readLine()) != null) {
+              String[] lines = line.split(" ");
+              if ((lines[0].equals("400") || lines[0].equals("500")) && isOn) {
+                  out.write(lines[1] + ";");
+                  isOn = false;
+              } else if (!isOn && (lines[0].equals("200") || lines[0].equals("300"))) {
+                  out.write(lines[1] + ";" + System.lineSeparator());
+              }
+          }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis anal = new Analysis();
+       anal.unavailable("./data/analysis/server.log", "./data/analysis/target.csv");
+    }
+}
