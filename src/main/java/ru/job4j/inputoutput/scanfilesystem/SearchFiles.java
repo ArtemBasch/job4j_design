c2c4bac568baba3616198b9dec9251f7cbd27a66
AremBasch <bashlykovartem@gmail.com>
2022-11-22 20:58:11 +0300

    Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/inputoutput/scanfilesystem/SearchFiles.java
@@ -0,0 +1,33 @@
+package ru.job4j.inputoutput.scanfilesystem;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    Predicate<Path> condition;
+    List<Path> paths = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file)) {
+            paths.add(file);
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+}
