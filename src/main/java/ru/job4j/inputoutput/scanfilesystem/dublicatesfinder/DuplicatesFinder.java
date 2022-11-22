79ac58b5244a8913dc104e760e9f5fc682a98db9
AremBasch <bashlykovartem@gmail.com>
2022-11-22 22:50:29 +0300

    Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/inputoutput/scanfilesystem/dublicatesfinder/DuplicatesFinder.java
+++ src/main/java/ru/job4j/inputoutput/scanfilesystem/dublicatesfinder/DuplicatesFinder.java
@@ -5,7 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DuplicatesFinder {
+
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("C:\\projects"), duplicatesVisitor);
+       for (Path paths : duplicatesVisitor.getDuplicates()) {
+           System.out.printf("Name %s size %d Kb \n Path %s \n", paths.toFile().getName(), paths.toFile().length() / 1024, paths.toFile().getAbsolutePath());
+       }
     }
 }
--- src/main/java/ru/job4j/inputoutput/scanfilesystem/dublicatesfinder/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/inputoutput/scanfilesystem/dublicatesfinder/DuplicatesVisitor.java
@@ -5,11 +5,27 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private HashMap<FileProperty, Path> files = new HashMap<>();
+    private List<Path> duplicates = new ArrayList<>();
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        FileProperty fProp = new FileProperty(file.toFile().length(), file.toFile().getName());
+
+        if (!files.containsKey(fProp)) {
+            files.put(fProp, file);
+        } else {
+            duplicates.add(file);
+        }
         return super.visitFile(file, attrs);
+    }
+
+    public List<Path> getDuplicates() {
+        return duplicates;
     }
 }
