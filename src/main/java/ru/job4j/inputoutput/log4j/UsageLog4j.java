eafa8681045d689c83e8e345b30ce3edc4ad10a7
AremBasch <bashlykovartem@gmail.com>
2023-01-16 23:10:39 +0300

    Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/inputoutput/log4j/UsageLog4j.java
+++ src/main/java/ru/job4j/inputoutput/log4j/UsageLog4j.java
@@ -7,10 +7,15 @@ public class UsageLog4j {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int maxInt = 2147483647;
+        boolean defaultValue = false;
+        byte maxByte = 127;
+        short maxShort = 32767;
+        long maxLong = 9223372036854775807L;
+        float maxFloat = 3.4E+38f;
+        double maxDouble = 1.7E+308d;
+        char maxChar = '\u0000';
+        LOG.debug("Primitive types: int: {},\n boolean: {},\n byte: {},\n short: {},\n long: {},\n float: {},\n double: {},\n char: {}",
+                maxInt, defaultValue, maxByte, maxShort, maxLong, maxFloat, maxDouble, maxChar);
     }
 }
