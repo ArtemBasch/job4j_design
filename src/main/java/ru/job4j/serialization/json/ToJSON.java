7ea2b8b2fbc8548d0a28e3cc0ca21237ca7d97e2
ArtemBasch <bashlykovartem@gmail.com>
2023-02-22 23:31:15 +0300

    Преобразование JSON в POJO. JsonObject [#315064]

---
+++ src/main/java/ru/job4j/serialization/json/ToJSON.java
@@ -0,0 +1,24 @@
+package ru.job4j.serialization.json;
+
+import org.json.JSONObject;
+import ru.job4j.serialization.Contact;
+import ru.job4j.serialization.Person;
+
+public class ToJSON {
+    public static void main(String[] args) {
+        Contact contact = new Contact(111111,"11-111");
+        String[] status = new String[] {"Worker", "Married"};
+        final Person person = new Person(false, 30, contact, status);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.isSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("zipCode", contact.getZipCode());
+        jsonObject.put("phone", contact.getPhone());
+        jsonObject.put("status", status);
+
+        System.out.println(jsonObject);
+        System.out.println(new JSONObject(person));
+
+
+    }
+}
